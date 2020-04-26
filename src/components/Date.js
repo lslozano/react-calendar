975218588144f0a50dff29dd933f56d188f12246
Luis Lozano <lslozano11@gmail.com>
2020-04-26 12:49:06 -0500

    Complete calendar view with last month days and preview of next month days

--- src/components/Date.js
+++ src/components/Date.js
@@ -7,15 +7,67 @@ const Date = () => {
     return firstDate
   }
 
-  const blankSquare = () => {
-    let square = []
+  const endOfMonth = () => {
+    let lastDate = moment().endOf('month').format('d')
+    return parseInt(lastDate)
+  }
+
+  const prevMonthDays = () => {
+    let prevMonthLastDays = []
+    let prevMonthDaysSquares = []
 
     for (let i = 0; i < monthStartDate(); i++) {
-      square.push(
-        <td>{''}</td>
+      prevMonthLastDays.push(moment().startOf('month').subtract(i + 1, 'day').format('D'))
+      prevMonthLastDays.sort()
+    }
+
+    for (let i = 0; i < monthStartDate(); i++) {
+      prevMonthDaysSquares.push(
+        <td key={moment().startOf('month').subtract(i, 'day')}>{prevMonthLastDays[i]}</td>
       )
     }
-    return square
+
+    return prevMonthDaysSquares
+  }
+
+  const nextMonthDays = () => {
+    let nextMonthFirstDays = []  
+    let nextMonthDaysSquares = []
+
+    switch(endOfMonth()) {
+      case 0:
+        nextMonthFirstDays = [1, 2, 3, 4, 5, 6]
+        break;
+      case 1:
+        nextMonthFirstDays = [1, 2, 3, 4, 5]
+        break;
+      case 2:
+        nextMonthFirstDays = [1, 2, 3, 4]
+        break;
+      case 3:
+        nextMonthFirstDays = [1, 2, 3]
+        break;
+      case 4:
+        nextMonthFirstDays = [1, 2]
+        break;
+      case 5:
+        nextMonthFirstDays = [1]
+        break;
+      case 6:
+        nextMonthFirstDays = []
+        break;
+      default:
+        nextMonthFirstDays = []
+        break;
+    }
+
+    for (let i = 0; i < nextMonthFirstDays.length; i++) {
+      nextMonthDaysSquares.push(
+        <td key={moment().endOf('month').add(i + 1, 'day')}>{nextMonthFirstDays[i]}</td>
+      )
+    }
+
+    return nextMonthDaysSquares
   }
 
   const daysOfMonth = () => {
@@ -37,10 +89,9 @@ const Date = () => {
   }
 
   const rows = () => {
-    const blocks = [...blankSquare(),...days()]
+    const blocks = [...prevMonthDays(),...days(),...nextMonthDays()]
     let rows = []
     let cells = [] 
-
     blocks.forEach((row, i) => {
       if (i % 7 !== 0) {
         cells.push(row)
@@ -54,6 +105,7 @@ const Date = () => {
         rows.push(cells)
       }
     })
+
     return rows
   }
 
